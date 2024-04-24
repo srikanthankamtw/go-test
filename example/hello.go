58bc430dbbf36cb12682ae3cf402d1465d7230ce
mindfreakersatya <pillakantri.srikanth@gmail.com>
2024-04-24 23:48:35 +0530

    add a some more tests to hello world

--- example/hello.go
+++ example/hello.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello World!"
+const (
+	englishHelloPrefix = "Hello"
+	spanishHelloPrefix = "Hola"
+	frenchHelloPrefix  = "Bonjour"
+	spaceDelimiter     = " "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return getLanguage(language) + spaceDelimiter + name
+}
+
+func getLanguage(language string) string {
+	languageHelloPrefix := englishHelloPrefix
+	switch language {
+	case "French":
+		languageHelloPrefix = frenchHelloPrefix
+	case "Spanish":
+		languageHelloPrefix = spanishHelloPrefix
+	}
+	return languageHelloPrefix
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Satya", "Spanish"))
 }
--- example/hello_test.go
+++ example/hello_test.go
@@ -5,8 +5,34 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello World!"
+	t.Run("should return Hola to people when name is not empty and language is spanish", func(t *testing.T) {
+		name := "Satya"
+		got := Hello(name, "Spanish")
+		want := "Hola Satya"
+		if got != want {
+			t.Errorf("want %q got %q", want, got)
+		}
+	})
+
+	t.Run("should return Bonjour to people when name is not empty and language is spanish", func(t *testing.T) {
+		name := "Satya"
+		got := Hello(name, "French")
+		want := "Bonjour Satya"
+		if got != want {
+			t.Errorf("want %q got %q", want, got)
+		}
+	})
+
+	t.Run("should return Hello World when name and language is passed an empty string", func(t *testing.T) {
+		name := ""
+		got := Hello(name, "")
+		want := "Hello World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("want %q got %q", want, got)
 	}
